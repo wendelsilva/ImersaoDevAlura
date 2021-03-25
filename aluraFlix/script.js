7250a64d5e23014d43f847e03f9b218c37965406
wendel245 <48558013+wendel245@users.noreply.github.com>
2021-03-25 16:15:24 -0300

    created a aluraflix with array

---
+++ aluraFlix/script.js
@@ -0,0 +1,17 @@
+/*
+var filmes = ["Star Wars", " Toy Store", " Interestellar", " Esqueceram de mim", " Marley e Eu"]
+
+for (var i = 2; i >= 0; i--){
+    alert(filmes[i])
+}
+
+for (var i = 0; i < filmes.length; i++){
+    alert(filmes[i])
+}
+*/
+
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++){
+    document.write("<img src=" + listaFilmes[i] + ">")
+}
